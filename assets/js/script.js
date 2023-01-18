d52f1f9776a607639b72e9992c4b1a1c200b9317
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-18 16:30:05 -0300

    criação do código javascript para interação com tela de navegação

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+const clickMenu = document.querySelector('.iconMenu');
+const clickClose = document.querySelector('.iconExit');
+const clickOpenFeat = document.querySelector('.featiconDown');
+const clickCloseFeat = document.querySelector('.featiconUp');
+const clickOpenComp = document.querySelector('.compiconDown');
+const clickCloseComp = document.querySelector('.compiconUp');
+const pageMenu = document.querySelector('.navigation');
+const pageFeatures = document.querySelector('.featlistSecond');
+const pageCompany = document.querySelector('.complist');
+
+
+function openMenu(){
+  pageMenu.style.display = 'flex';
+  pageMenu.style.width = '63%'
+}
+
+function closeMenu(){
+  pageMenu.style.display = 'none';
+  pageMenu.style.width = 0
+}
+
+function openFeatures(){
+  clickCloseFeat.style.display = 'block';
+  clickOpenFeat.style.display = 'none';  
+  pageFeatures.style.display = 'block';
+}
+
+function closeFeatures(){
+  clickCloseFeat.style.display = 'none';
+  clickOpenFeat.style.display = 'block';  
+  pageFeatures.style.display = 'none';
+}
+
+function openCompany(){
+  clickCloseComp.style.display = 'block';
+  clickOpenComp.style.display = 'none';
+  pageCompany.style.display = 'block';
+}
+
+function closeCompany(){
+  clickOpenComp.style.display = 'block';
+  clickCloseComp.style.display = 'none';  
+  pageCompany.style.display = 'none';
+}
+
+clickMenu.addEventListener('click',openMenu);
+clickClose.addEventListener('click',closeMenu);
+clickOpenFeat.addEventListener('click',openFeatures);
+clickCloseFeat.addEventListener('click',closeFeatures);
+clickOpenComp.addEventListener('click',openCompany);
+clickCloseComp.addEventListener('click',closeCompany);
