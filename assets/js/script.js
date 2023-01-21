7671d1eb11bfd36c2c7fc5b5241591b7fb560ea5
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-21 11:30:43 -0300

    criado, estilizado e inserido código js do  submenu da opção features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ const openbtn = document.querySelector('.btn-open');
 const closebtn = document.querySelector('.btn-close');
 const navbtn = document.querySelector('.nav-btn');
 const navmenu = document.querySelector('.nav-menu');
+const submenufeat = document.querySelector('.submenu-feat');
+const featbtn = document.querySelector('.feat');
 
 openbtn.addEventListener('click', () =>{
   openbtn.classList.toggle('active');
@@ -16,4 +18,8 @@ closebtn.addEventListener('click',() =>{
   closebtn.classList.toggle('active');
   navmenu.classList.toggle('active');
   navbtn.classList.toggle('active');
+})
+
+featbtn.addEventListener('click',() =>{
+  submenufeat.classList.toggle('active');
 })
