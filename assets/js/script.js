6e76c70791ddb8d9d4e22954ebc43a56d4aa0466
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-21 12:23:22 -0300

    finalização do código javascript: ativação dos submenus, funcionalidade dos submenus

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@ const closebtn = document.querySelector('.btn-close');
 const navbtn = document.querySelector('.nav-btn');
 const navmenu = document.querySelector('.nav-menu');
 const submenufeat = document.querySelector('.submenu-feat');
+const submenucomp = document.querySelector('.submenu-comp');
 const featbtn = document.querySelector('.feat');
+const compbtn = document.querySelector('.comp');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
 openbtn.addEventListener('click', () =>{
   openbtn.classList.toggle('active');
@@ -18,8 +22,27 @@ closebtn.addEventListener('click',() =>{
   closebtn.classList.toggle('active');
   navmenu.classList.toggle('active');
   navbtn.classList.toggle('active');
+  submenucomp.classList.remove('active');
+  submenufeat.classList.remove('active');
 })
 
 featbtn.addEventListener('click',() =>{
   submenufeat.classList.toggle('active');
+  submenucomp.classList.remove('active');
 })
+
+compbtn.addEventListener('click',() =>{
+  submenucomp.classList.toggle('active');
+  submenufeat.classList.remove('active');
+})
+
+main.addEventListener('click',()=>{
+  submenucomp.classList.remove('active');
+  submenufeat.classList.remove('active');
+})
+
+footer.addEventListener('click',()=>{
+  submenucomp.classList.remove('active');
+  submenufeat.classList.remove('active');
+})
+
